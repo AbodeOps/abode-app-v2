e26e01e41c256dee731bb7b4d2a2113676d189c0
Gideon Peters <gideonpeters85@gmail.com>
2022-09-09 03:24:49 +0100

    feat: remove unused component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <template>
-	<AuthNavbar />
 	<hr />
 	<Navbar />
 </template>
 
 <script setup lang="ts">
-import AuthNavbar from '@/components/layouts/AuthNavbar.vue';
 import Navbar from '@/components/layouts/Navbar.vue';
 </script>
