6332420cdb1b94d1db69b648348e553b25f9de78
Gideon Peters <gideonpeters85@gmail.com>
2022-10-12 01:39:57 +0100

    feat: mobile adaption

---
+++ android/app/src/main/java/com/myabode/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myabode.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myabode.app',
+  appName: 'Abode',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
