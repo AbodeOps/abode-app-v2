f35d5a9b2e634ccd65fc8fd254eb0eaef6413880
Omotayo Bakare <isaacbyles@gmail.com>
2023-07-17 14:12:37 +0100

    removed hardcoded changes

--- src/components/assets/AssetMoreDetailsCard.vue
+++ src/components/assets/AssetMoreDetailsCard.vue
@@ -4,7 +4,6 @@
 		<div class="mt-8 grid grid-cols-2 gap-4 md:grid-cols-3">
 			<AssetField :value="`${asset.totalUnits}`" label="Total Units" />
 			<AssetField :value="`${asset.available_units}`" label="Available Units" />
-		
 			<AssetField :value="`${asset.potentialGrowth}`" label="Potential Capital Appreciation" />
 
 		</div>
